c6c7a12c497b756057354aa782ed71fae3d2f67e
lavaboy1738 <yanzfeng@bu.edu>
2020-05-07 18:09:16 -0400

    changing pictures to links so github can finally host it, hopefully

--- songs.js
+++ songs.js
@@ -5,7 +5,7 @@ const songsList = [
         title: "Don't HMU",
         artist: "Savantloup",
         time: "2:43",
-        cover: "/pictures/don't-hmu.jpg",
+        cover: "https://images.genius.com/b3aaf5ffd4cf4b4eab294086ad7949eb.1000x1000x1.jpg",
         color:"rgb(122, 112, 190)"
     },
 
@@ -15,7 +15,7 @@ const songsList = [
         title:"Cloud Hill",
         artist: "F5",
         time: "3:18",
-        cover:"/pictures/clout-hill.jpg",
+        cover:"https://i1.sndcdn.com/artworks-28upo8zYFyIfi1ZB-NMQ4BQ-t500x500.jpg",
         color:"rgb(120, 141, 46)"
     },
 
@@ -25,7 +25,7 @@ const songsList = [
         title: "STARGAZING",
         artist: "Travis Scott",
         time: "4:32",
-        cover: "/pictures/stargazing.jpg",
+        cover: "https://images.genius.com/9c8508d3056b146aee2ad72d5f0606e7.926x926x1.jpg",
         color:"rgb(90, 152, 193)"
     },
 
@@ -35,7 +35,7 @@ const songsList = [
         title: "Anime Thighs",
         artist: "MC Virgin",
         time: "2:35",
-        cover: "/pictures/anime-thighs.jpg",
+        cover: "https://images.genius.com/bac3a0dd317844c6bf98f0896c75bdf2.1000x1000x1.jpg",
         color:"rgb(195, 138, 61)"
     },
 
@@ -45,7 +45,7 @@ const songsList = [
         title: "A lot",
         artist: "21 Savage feat. J. Cole",
         time: "4:49",
-        cover: "/pictures/a-lot.jpg",
+        cover: "https://images.genius.com/fdf55cd3f0aaf3b48e1437d99bdfbcd1.1000x1000x1.jpg",
         color:"rgb(184, 177, 174)"
     },
 
@@ -55,7 +55,7 @@ const songsList = [
         title: "Toosie Slide",
         artist: "Drake",
         time: "4:08",
-        cover: "./pictures/toosie-slide.png",
+        cover: "https://images.genius.com/c27f4115b093ba3744f4784ec6f03c29.1000x1000x1.png",
         color:"rgb(121, 163, 191)"
     },
 
@@ -65,7 +65,7 @@ const songsList = [
         title: "Through the Wire",
         artist: "Kanye West",
         time: "3:41",
-        cover: "/pictures/through-the-wire.jpg",
+        cover: "https://images.genius.com/0bffd93463afe53e7f651f72bedfc78b.1000x1000x1.jpg",
         color:"rgb(204, 142, 34)"
     },
 
@@ -75,7 +75,7 @@ const songsList = [
         title: "Stay Schemin'",
         artist: "Rick Ross feat. Drake",
         time: "4:28",
-        cover: "./pictures/stay-schemin.jpg",
+        cover: "https://images.genius.com/8696d51bb67d330c6690792e2d975c62.700x700x1.jpg",
         color:"rgb(204, 203, 195)"
     },
 
@@ -85,7 +85,7 @@ const songsList = [
         title: "Mask Off",
         artist: "Future",
         time: "3:25",
-        cover: "/pictures/mask-off.jpg",
+        cover: "https://images.genius.com/ee2a2f360d0b469d8cebfe6dc5a058d7.1000x994x1.jpg",
         color:"rgb(220, 197, 86)"
     },
 
@@ -95,7 +95,7 @@ const songsList = [
         title: "Fresh",
         artist: "Kool & the Gang",
         time: "4:24",
-        cover: "./pictures/fresh.jpg",
+        cover: "https://images.genius.com/2cb7d1f8e124a549624c53fa91624f95.1000x1000x1.jpg",
         color:"rgb(200, 84, 76)"
     },
 
@@ -105,7 +105,7 @@ const songsList = [
         title: "Dirty Money",
         artist: "Clipse",
         time: "3:45",
-        cover: "/pictures/dirty-money.jpg",
+        cover: "https://images.genius.com/7521267b7988b507395d65324d39651d.1000x1000x1.jpg",
         color:"rgb(202, 186, 166)"
     },
 
@@ -115,7 +115,7 @@ const songsList = [
         title: "Protect Ya Neck",
         artist: "Wu-Tang Clan",
         time: "5:03",
-        cover: "/pictures/protect-ya-neck.jpg",
+        cover: "https://images.genius.com/d73cfa7698dd285430e7fb0682f2d02e.1000x1000x1.jpg",
         color:"rgb(220, 197, 86)"
     },
 
@@ -125,7 +125,7 @@ const songsList = [
         title: "Easy Lover",
         artist: "Philip Bailey feat. Phil Collins",
         time: "5:06",
-        cover: "/pictures/easy-lover.jpg",
+        cover: "https://images.genius.com/8cf608268596d3f6da530c54a32cbbc4.1000x996x1.jpg",
         color:"rgb(107, 71, 66)"
     },
 
@@ -135,7 +135,7 @@ const songsList = [
         title: "Do Not Disturb",
         artist: "Drake",
         time: "4:44",
-        cover: "/pictures/do-not-disturb.jpg",
+        cover: "https://images.genius.com/492890e920533797aa7eccf68e467b8a.1000x1000x1.jpg",
         color:"rgb(15, 98, 164)"
     },
 
@@ -145,7 +145,7 @@ const songsList = [
         title: "Enormous Penis",
         artist: "Da Vinci's Notebook",
         time: "2:45",
-        cover: "/pictures/enormous-penis.png",
+        cover: "https://images.genius.com/450fa42aaf301d77867ed1ec74b96c49.999x999x1.png",
         color:"rgb(168, 207, 211)"
     }
 ]
